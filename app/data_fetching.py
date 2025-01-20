c70336f7e5b9bf1ee143a5128450aff1da23a2fe
florinho1503 <145715335+florinho1503@users.noreply.github.com>
2025-01-20 13:54:13 +0100

    Remove redundant live price fetching

--- app/data_fetching.py
+++ app/data_fetching.py
@@ -9,20 +9,3 @@ def fetch_historical_data(stock_ticker):
     data = stock.history(period="1y")  # Adjust period as needed
     return data[['Close']]  # Return closing prices
 
-
-def get_live_price(stock_ticker):
-    """voor live data scrapen we de website van yahoofinance, want dit kan niet (gratis) via de api"""
-
-    url = f"https://finance.yahoo.com/quote/{stock_ticker}/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-    }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    price_tag = soup.find("fin-streamer", {"data-field": "regularMarketPrice"})
-    price = float(price_tag.text.replace(",", ""))
-    return price
-
