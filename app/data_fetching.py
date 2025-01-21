248297c3a18ec03b4c0926bdb15f9bcddb85dac4
Floris Bokx <florisbokx@gmail.com>
2025-01-21 11:38:07 +0100

    change yfinance history closing prices from 1y to 5y

--- app/data_fetching.py
+++ app/data_fetching.py
@@ -4,6 +4,6 @@ import yfinance as yf
 def fetch_historical_data(stock_ticker):
     """Function to get historical price data of a stock (wij gebruiken alleen open/close)"""
     stock = yf.Ticker(stock_ticker)
-    data = stock.history(period="1y")
+    data = stock.history(period="5y")
     return data[['Close']]
 
